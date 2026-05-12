d0d9a373079f2aa55a18c160ffa75bf5296eea4e
felix <felixyeah@outlook.com>
2026-05-11 20:25:13 -0700

    feat: limit the leaderboard to the first 10 users

--- app/main/leaderboard.tsx
+++ app/main/leaderboard.tsx
@@ -1,14 +1,20 @@
 
 export function Leaderboard({users}: {users: [string, number][]}) {
+    const _users = users.slice(0, 10);
 
     return (
         <div id="leaderboard" className="leaderboard">
             <h1>Leaderboard:</h1>
             <div className="table">
-                {users.map(([userId, points], index) => (
+                {_users.map(([userId, points], index) => (
                     <p className="leaderboard-item" key={userId}>{index + 1}. {userId}: {points}</p>
                 ))}
             </div>
+            {
+                users.length > 10 ? (
+                    <p onClick={() => { window.location.href = "/leaderboard"}} className="leaderboard-item">See More...</p>
+                ) : null
+            }
         </div>
     )
 }
